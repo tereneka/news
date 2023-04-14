f84792a38b40efdbcdcb4492d9eba67658feb66f
tereneka <teren-eka@yandex.ru>
2023-04-15 00:08:52 +0300

    feat: add news list

--- src/features/news/NewsCard.tsx
+++ src/features/news/NewsCard.tsx
@@ -12,6 +12,10 @@ export default function NewsCard({
     <article>
       <cite>{data.source.name}</cite>
       <h3>{data.title}</h3>
+      {data.urlToImage && (
+        <img src={data.urlToImage} alt='img' />
+      )}
+
       <p>{data.description}</p>
     </article>
   );
